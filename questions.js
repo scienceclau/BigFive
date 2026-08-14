413d8ea0b51e95eccb1591364077fb0e080f70c0
scienceclau <clau@strategydoula.com>
2026-08-14 08:10:42 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,61 @@
+const questions = [
+  { id: 1, text: "Am the life of the party." },
+  { id: 2, text: "Feel little concern for others." },
+  { id: 3, text: "Am always prepared." },
+  { id: 4, text: "Get stressed out easily." },
+  { id: 5, text: "Have a rich vocabulary." },
+
+  { id: 6, text: "Don't talk a lot." },
+  { id: 7, text: "Am interested in people." },
+  { id: 8, text: "Leave my belongings around." },
+  { id: 9, text: "Am relaxed most of the time." },
+  { id: 10, text: "Have difficulty understanding abstract ideas." },
+
+  { id: 11, text: "Feel comfortable around people." },
+  { id: 12, text: "Insult people." },
+  { id: 13, text: "Pay attention to details." },
+  { id: 14, text: "Worry about things." },
+  { id: 15, text: "Have a vivid imagination." },
+
+  { id: 16, text: "Keep in the background." },
+  { id: 17, text: "Sympathize with others' feelings." },
+  { id: 18, text: "Make a mess of things." },
+  { id: 19, text: "Seldom feel blue." },
+  { id: 20, text: "Am not interested in abstract ideas." },
+
+  { id: 21, text: "Start conversations." },
+  { id: 22, text: "Am not interested in other people's problems." },
+  { id: 23, text: "Get chores done right away." },
+  { id: 24, text: "Am easily disturbed." },
+  { id: 25, text: "Have excellent ideas." },
+
+  { id: 26, text: "Have little to say." },
+  { id: 27, text: "Have a soft heart." },
+  { id: 28, text: "Often forget to put things back in their proper place." },
+  { id: 29, text: "Get upset easily." },
+  { id: 30, text: "Do not have a good imagination." },
+
+  { id: 31, text: "Talk to a lot of different people at parties." },
+  { id: 32, text: "Am not really interested in others." },
+  { id: 33, text: "Like order." },
+  { id: 34, text: "Change my mood a lot." },
+  { id: 35, text: "Am quick to understand things." },
+
+  { id: 36, text: "Don't like to draw attention to myself." },
+  { id: 37, text: "Take time out for others." },
+  { id: 38, text: "Shirk my duties." },
+  { id: 39, text: "Have frequent mood swings." },
+  { id: 40, text: "Use difficult words." },
+
+  { id: 41, text: "Don't mind being the center of attention." },
+  { id: 42, text: "Feel others' emotions." },
+  { id: 43, text: "Follow a schedule." },
+  { id: 44, text: "Get irritated easily." },
+  { id: 45, text: "Spend time reflecting on things." },
+
+  { id: 46, text: "Am quiet around strangers." },
+  { id: 47, text: "Make people feel at ease." },
+  { id: 48, text: "Am exacting in my work." },
+  { id: 49, text: "Often feel blue." },
+  { id: 50, text: "Am full of ideas." }
+];
