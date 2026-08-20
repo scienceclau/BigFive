ebd04c545a9a3e5a1ca8007633b18017e9923b60
scienceclau <clau@strategydoula.com>
2026-08-20 14:52:47 -0400

    Update results.js
    Change to 100 scale

--- results.js
+++ results.js
@@ -5,9 +5,16 @@ function buildResultsProfile(scoredResults) {
     const rawScore = scoredResults[trait].rawScore;
     const meanScore = scoredResults[trait].meanScore;
 
+    // Convert possible raw-score range (10–50)
+    // to a 0–100 scale.
+    const scaleScore = Math.round(
+      ((rawScore - 10) / 40) * 100
+    );
+
     profile[trait] = {
       rawScore: rawScore,
-      meanScore: meanScore
+      meanScore: meanScore,
+      scaleScore: scaleScore
     };
   }
 
